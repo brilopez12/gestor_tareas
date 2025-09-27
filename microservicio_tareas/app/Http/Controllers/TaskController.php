a49acdd1fbf93f5f8da199ae65ee2cd930d17051
Brigith López <>
2025-09-27 14:51:34 -0500

    Microservicio de tareas

---
+++ microservicio_tareas/app/Http/Controllers/TaskController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Task;
+
+
+class TaskController extends Controller
+{
+    public function index()
+    {
+        return Task::all();
+    }
+
+    public function store(Request $request)
+    {
+        $task = Task::create($request->all());
+        return response()->json($task, 201);
+    }
+
+    public function show($id)
+    {
+        return Task::findOrFail($id);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $task = Task::findOrFail($id);
+        $task->update($request->all());
+        return response()->json($task, 200);
+    }
+
+    public function destroy($id)
+    {
+        Task::destroy($id);
+        return response()->json(null, 204);
+    }
+}
+
+
