c1b6356fcca50268c5710f222727b26b27d81fbc
brilopez12 <161358568+brilopez12@users.noreply.github.com>
2025-10-14 20:27:35 -0500

    notificaciones_locustfile.py
    Removed comment about the microservice endpoint.

--- pruebas/locustfiles/notificaciones_locustfile.py
+++ pruebas/locustfiles/notificaciones_locustfile.py
@@ -3,7 +3,6 @@ from locust import HttpUser, task
 class NotificacionesUser(HttpUser):
     @task
     def notificar(self):
-        # Endpoint del microservicio
         with self.client.get("/notificar", catch_response=True) as response:
             if response.status_code == 200:
                 response.success()
