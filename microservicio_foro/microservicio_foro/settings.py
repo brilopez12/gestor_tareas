7b2b1e9180cd9a4a8a531f2e59e296084c2c3b78
brilopez12 <blopezfl@unal.edu.co>
2025-11-18 12:59:29 -0500

    microserviciio_notificaciones

--- microservicio_foro/microservicio_foro/settings.py
+++ microservicio_foro/microservicio_foro/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -77,11 +77,11 @@ WSGI_APPLICATION = 'microservicio_foro.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'foro',
-        'USER': 'root',
-        'HOST': 'localhost',
-        'PORT': '3306',
-
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST'),
+        'PORT': os.environ.get('DB_PORT'),
     }
 }
 
